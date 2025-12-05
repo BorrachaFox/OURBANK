69a24c1c4dc76018674a661c17501979a9892931
rafaeldecothe <rafael.decothe@gmail.com>
2025-12-05 12:04:56 -0300

    implementação da tela de perfil

---
+++ app/src/main/java/com/example/ourbank/TelaPerfilUsuario.kt
@@ -0,0 +1,14 @@
+package com.example.ourbank
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TelaPerfilUsuario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Define o layout XML como a view desta Activity
+        setContentView(R.layout.tela_escolha_conta)
+
+        //Aqui adiciona lógica de clique
+    }
+}
